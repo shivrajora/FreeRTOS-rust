cb5175b4f1fe614272ff96cbb399b79795114e2a
Shiv Rajora <rajora.shiv@gmail.com>
2026-03-07 07:05:44 -0800

    Add suport for Cortex M0/M0+ targets (eg. RP2040)
    - Cortex M0/M0+ do not have hardware to perform atomic operations,
      therefore software atomics need to be added.

--- freertos-rust/src/prelude/no_std.rs
+++ freertos-rust/src/prelude/no_std.rs
@@ -7,5 +7,8 @@ pub use core::ops::{Deref, DerefMut};
 
 pub use alloc::boxed::Box;
 pub use alloc::string::*;
+#[cfg(target_has_atomic = "ptr")]
 pub use alloc::sync::{Arc, Weak};
+#[cfg(not(target_has_atomic = "ptr"))]
+pub use portable_atomic_util::{Arc, Weak};
 pub use alloc::vec::Vec;
